663291f516e4364c985bea55608769d7593305d4
zhuting <zhuting0574@gmail.com>
2026-09-23 00:49:49 +0800

    chore: use local API for mini program debugging

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,5 @@
 App({
-  globalData: { apiBase: 'https://api.milsimnb.cn/api', token: '', user: null, pendingRoute: '' },
+  globalData: { apiBase: 'http://localhost:8081/api', token: '', user: null, pendingRoute: '' },
   onLaunch() {
     this.globalData.token = wx.getStorageSync('token') || ''
     this.globalData.user = wx.getStorageSync('user') || null
